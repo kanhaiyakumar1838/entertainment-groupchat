64a2e5bfccb8eea3462ec94eeb6b7caf7953039f
kanhaiyakumar1838 <97240685+kanhaiyakumar1838@users.noreply.github.com>
2025-10-23 23:26:56 +0530

    modified  frontend authcontext

--- Frontend/src/context/AuthContext.js
+++ Frontend/src/context/AuthContext.js
@@ -5,7 +5,10 @@ export const AuthContext = createContext();
 
 // Context Provider Component
 export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
 
   const login = (data) => {
     localStorage.setItem("token", data.token);
