113ed9e37203dcb06d545e87dbf468ab2d4d5a8c
kanhaiyakumar1838 <97240685+kanhaiyakumar1838@users.noreply.github.com>
2025-10-24 21:20:22 +0530

    modifies server.js

--- Backend/server.js
+++ Backend/server.js
@@ -48,7 +48,8 @@ mongoose
   .then(() => {
     console.log("✅ MongoDB connected");
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+    app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server running on port ${PORT}`));
+
   })
   .catch((err) => {
     console.error("❌ MongoDB connection error:", err);
